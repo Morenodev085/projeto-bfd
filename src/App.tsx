a8cbc1e38fb59330cdf78c06507ede13cd4aefb8
Morenodev085 <moreno.ferreira11@gmail.com>
2026-01-07 13:54:36 -0300

    ..

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { GlobalStyles } from "./globalStyled"
+
+
+function App() {
+
+  return (
+    <>
+    <GlobalStyles/>
+      <h1>Meu projeto esta funcionando!
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/globalStyled.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components"
+
+export const theme = {
+    colors:{
+        primary: #fff,
+        secondary: #fff,
+        backgroud: #fff,
+        text: #000000,
+
+    },
+};
+
+
+
+export const GlobalStyles = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+
+`
